23c8a593e39eb89df23738823a4e6a394a376265
Paymahn Moghadasian <paymahn@paymahns-mbp.gateway.mts.net>
2014-11-25 16:28:06 -0600

    Initial commit. Open up to lauren.
    Code is ugly. I probably won't fix or clean it up anytime soon.
    
    As of right now we can:
    - Read in the data
    - Clean the data
    - Create histograms
    - Extract a column from the data

---
+++ analysis.py
@@ -0,0 +1,220 @@
+import numpy as np
+from matplotlib import pyplot as py
+from datetime import datetime
+
+
+def remove_accept_columns(row):
+    del row[50]
+    del row[48]
+    del row[47]
+    del row[45]
+    del row[33]
+    del row[29]
+    del row[28]
+    del row[26]
+    del row[22]
+    del row[19]
+    del row[18]
+    del row[1]
+    del row[0]
+
+
+def remove_reject_columns(row):
+    del row[5]
+
+
+def load_accept_data(file_name):
+    data = []
+    income_status_dict = {"Verified": 0, "Not Verified": 1, "Source Verified": 2}
+    home_ownership_dict = {'RENT': 0, 'MORTGAGE': 1, 'OWN': 2, 'OTHER': 4, 'NONE': 5}
+    loan_status_dict = {'Charged Off': 0, 'Default': 1, 'Issued': 2, 'Fully Paid': 3, 'Current': 4,
+                        'Late (16-30 days)': 5, 'Late (31-120 days)': 6, 'In Grace Period': 7}
+    state_dict = {"AL": 0, "AK": 1, "AZ": 2, "AR": 3, "CA": 4, "CO": 5, "CT": 6, "DE": 7, "DC": 8, "FL": 9, "GA": 10,
+                  "HI": 11, "ID": 12, "IL": 13, "IN": 14, "IA": 15, "KS": 16, "KY": 17, "LA": 18, "MD": 19, "MA": 20,
+                  "MI": 21, "MN": 22, "MS": 23, "MO": 24, "MT": 25, "NE": 26, "NV": 27, "NH": 28, "NJ": 29, "NM": 30,
+                  "NY": 31, "NC": 32, "OH": 33, "OK": 34, "OR": 35, "PA": 36, "RI": 37, "SC": 38, "SD": 39, "TN": 40,
+                  "TX": 41, "UT": 42, "VT": 43, "VA": 44, "WA": 45, "WV": 46, "WI": 47, "WY": 48}
+
+    with open(file_name) as f:
+        column_labels = map(lambda x: x.strip("\" \n"), f.readline().split("\",\""))
+        remove_accept_columns(column_labels)
+
+        for line in f:
+            d = map(lambda x: x.strip("\" \n"), line.split("\",\""))
+
+            d[:5] = map(int, d[:5])  # cast first 5 columns to ints
+            d[5] = int(d[5][:2])
+            d[6] = float(d[6][:-1])
+            d[7] = float(d[7])
+            d[8] = ord(d[8][0]) - ord('A')
+            d[9] = int(d[9][1])
+
+            if d[11] == "n/a" or d[11][0] == '<':
+                temp = 0
+            elif d[11][1] == '0':
+                temp = 10
+            else:
+                temp = int(d[11][0])
+            d[11] = temp
+            d[12] = home_ownership_dict[d[12]]
+            d[13] = float(d[13])
+            d[14] = income_status_dict[d[14]]
+            d[15] = datetime.strptime(d[15], "%b-%Y")
+            d[16] = loan_status_dict[d[16]]
+            d[17] = ["n", "y"].index(d[17])
+            d[23] = state_dict[d[23]]
+            d[24] = float(d[24])
+            d[25] = int(d[25])
+            d[26] = datetime.strptime(d[26], "%b-%Y")
+            d[27] = int(d[27])
+            # vars 28 and 29 seem to not have incomplete data
+            d[30] = int(d[30])
+            d[31] = int(d[31])
+            d[32] = int(d[32])
+            # 33 has incomplete data
+            d[34] = int(d[34])
+            d[36:42] = map(float, map(lambda x: x.replace(",", ""), d[36:42]))
+            d[42:45] = map(float, d[42:45])
+            # 45 has incomplete data
+            d[46] = float(d[46].replace(",", ""))
+            # 47 and 48 have incomplete data
+            d[49] = int(d[49])
+            d[51] = int(d[51])
+
+            remove_accept_columns(d)
+            data.append(d)
+
+    return column_labels, data
+
+
+def load_reject_data(file_name):
+    data = []
+    with open(file_name) as f:
+        column_labels = map(lambda x: x.strip("\" \n"), f.readline().split(","))
+        remove_reject_columns(column_labels)
+
+        for i, line in enumerate(f):
+            d = map(lambda x: x.strip("\" \n"), line.split("\",\""))
+            try:
+                d[0] = float(d[0])
+                d[3] = int(d[3])
+                d[4] = float(d[4][:-1])
+
+                if d[7] == "n/a" or d[7][0] == '<':
+                    temp = 0
+                elif d[7][1] == '0':
+                    temp = 10
+                else:
+                    temp = int(d[7][0])
+                d[7] = temp
+
+                d[8] = int(d[8])
+
+                remove_reject_columns(d)
+                data.append(d)
+            except ValueError:
+                pass
+
+    return column_labels, data
+
+
+def extract_column(data, index):
+    return np.array([row[index] for row in data])
+
+
+def add_column(data, column):
+    for row, value in zip(data, column):
+        row.append(value)
+
+
+def generate_histograms(data, column_index, column_name, bin_counts=[10, 20, 30, 40, 50],
+                        remove_extreme_values=False, remove_count=100, remove_from_lower=True, normalize=False):
+    histogram_data = extract_column(data, column_index)
+
+    print "Min (before removal)= %d" % min(histogram_data)
+    print "Max (before removal)= %d" % max(histogram_data)
+
+    if remove_extreme_values:
+        if remove_from_lower:
+            histogram_data = sorted(histogram_data)[remove_count:]
+        else:
+            histogram_data = sorted(histogram_data)[:-1 * remove_count]
+
+        print "Min (after removal)= %d" % min(histogram_data)
+        print "Max (after removal)= %d" % max(histogram_data)
+
+    for bin_count in bin_counts:
+        py.hist(histogram_data, bin_count, alpha=0.75, normed=normalize)
+        py.title(column_name)
+        py.xlim([min(histogram_data) - 1, max(histogram_data) + 1])
+        py.show()
+
+
+def generate_normalized_state_histogram(data):
+    state_populations_dict = {"AL": 4833722, "AK": 735132, "AZ": 6626624, "AR": 2959373, "CA": 38332521, "CO": 5268367,
+                              "CT": 3596080, "DE": 925749, "DC": 646449, "FL": 19552860, "GA": 9992167, "HI": 1404054,
+                              "ID": 1612136, "IL": 12882135, "IN": 6570902, "IA": 3090416, "KS": 2893957, "KY": 4395295,
+                              "LA": 4625470, "MD": 5928814, "MA": 6692824, "MI": 9895622, "MN": 5420380, "MS": 2991207,
+                              "MO": 6044171, "MT": 1015165, "NE": 1868516, "NV": 2790136, "NH": 1323459, "NJ": 8899339,
+                              "NM": 2085287, "NY": 19651127, "NC": 9848060, "OH": 11570808, "OK": 3850568,
+                              "OR": 3930065, "PA": 12773801, "RI": 1051511, "SC": 4774839, "SD": 844877, "TN": 6495978,
+                              "TX": 26448193, "UT": 2900872, "VT": 626630, "VA": 8260405, "WA": 6971406, "WV": 1854304,
+                              "WI": 5742713, "WY": 582658}
+
+    state_dict = {"AL": 0, "AK": 1, "AZ": 2, "AR": 3, "CA": 4, "CO": 5, "CT": 6, "DE": 7, "DC": 8, "FL": 9, "GA": 10,
+                  "HI": 11, "ID": 12, "IL": 13, "IN": 14, "IA": 15, "KS": 16, "KY": 17, "LA": 18, "MD": 19, "MA": 20,
+                  "MI": 21, "MN": 22, "MS": 23, "MO": 24, "MT": 25, "NE": 26, "NV": 27, "NH": 28, "NJ": 29, "NM": 30,
+                  "NY": 31, "NC": 32, "OH": 33, "OK": 34, "OR": 35, "PA": 36, "RI": 37, "SC": 38, "SD": 39, "TN": 40,
+                  "TX": 41, "UT": 42, "VT": 43, "VA": 44, "WA": 45, "WV": 46, "WI": 47, "WY": 48}
+
+    histogram_data = extract_column(data, 18)
+    a, b = np.histogram(histogram_data, np.arange(50) - 0.5)
+
+    print a
+    print sum(a)
+
+    normalized = []
+    for state in state_dict.keys():
+        normalized_value = a[state_dict[state]]*1000 / float(state_populations_dict[state])
+        normalized.append(normalized_value)
+
+    py.bar(range(49), normalized, align='center')
+    py.xticks(range(50))
+    py.xlim([-1, 50])
+    py.ylim([0, 1])
+
+    py.show()
+
+
+def main():
+    col, data = load_accept_data("LoanStats3b.csv")
+
+
+    # accept histograms
+    # -----------------
+    # generate_histograms(data, 0, col[0])
+    # generate_histograms(data, 11, col[11], remove_extreme_values=True, remove_count=1000, remove_from_lower=False)
+    # generate_histograms(data, 22, col[22])
+    # generate_histograms(data, 18, col[18], bin_counts=[np.arange(50) - 0.5])
+    generate_histograms(data, 9, col[9], bin_counts=[np.arange(12) - 0.5], normalize=True)
+
+
+    # reject histograms
+    # -----------------
+    # col, data = load_reject_data("RejectStatsA.csv")
+    # generate_histograms(data, 6, col[6], bin_counts=[np.arange(12) - 0.5], normalize=True)
+
+    # blah = set()
+    # for row in data:
+    #     blah.add(row[9])
+    #
+    # print blah
+    #
+    # d = {a: b for a, b in zip(blah, range(len(blah)))}
+    # print d
+
+    # print len(load_reject_data("RejectStatsA.csv"))
+
+
+if __name__ == "__main__":
+    main()
