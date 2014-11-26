ca4c890d529163111a4dbbe7cd0eb4267569f4a0
Paymahn Moghadasian <paymahn1@gmail.com>
2014-11-25 20:43:37 -0600

    Cleanup
    Cleaned up the code a bit. Added some comments. Cleaned up the data.
    
    Now, instead of loading the shitty data provided by lending club we can
    load data that has been previously cleaned. This cuts on on file size for
    the data files which has several pros: loading is faster and can put files
    here in git to keep track of changes.

--- analysis.py
+++ analysis.py
@@ -1,6 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as py
 from datetime import datetime
+from sklearn.cluster import KMeans
+from sklearn.tree import DecisionTreeClassifier
 
 
 def remove_accept_columns(row):
@@ -23,17 +25,16 @@ def remove_reject_columns(row):
     del row[5]
 
 
-def load_accept_data(file_name):
+def load_original_accept_data(file_name, state_dict):
+    """
+    Meant to read in the data file provided by lending club and clean it. There's another method below
+    which is meant to read the file created after writing the cleaned version of this file
+    """
     data = []
     income_status_dict = {"Verified": 0, "Not Verified": 1, "Source Verified": 2}
     home_ownership_dict = {'RENT': 0, 'MORTGAGE': 1, 'OWN': 2, 'OTHER': 4, 'NONE': 5}
     loan_status_dict = {'Charged Off': 0, 'Default': 1, 'Issued': 2, 'Fully Paid': 3, 'Current': 4,
                         'Late (16-30 days)': 5, 'Late (31-120 days)': 6, 'In Grace Period': 7}
-    state_dict = {"AL": 0, "AK": 1, "AZ": 2, "AR": 3, "CA": 4, "CO": 5, "CT": 6, "DE": 7, "DC": 8, "FL": 9, "GA": 10,
-                  "HI": 11, "ID": 12, "IL": 13, "IN": 14, "IA": 15, "KS": 16, "KY": 17, "LA": 18, "MD": 19, "MA": 20,
-                  "MI": 21, "MN": 22, "MS": 23, "MO": 24, "MT": 25, "NE": 26, "NV": 27, "NH": 28, "NJ": 29, "NM": 30,
-                  "NY": 31, "NC": 32, "OH": 33, "OK": 34, "OR": 35, "PA": 36, "RI": 37, "SC": 38, "SD": 39, "TN": 40,
-                  "TX": 41, "UT": 42, "VT": 43, "VA": 44, "WA": 45, "WV": 46, "WI": 47, "WY": 48}
 
     with open(file_name) as f:
         column_labels = map(lambda x: x.strip("\" \n"), f.readline().split("\",\""))
@@ -87,7 +88,33 @@ def load_accept_data(file_name):
     return column_labels, data
 
 
-def load_reject_data(file_name):
+def load_clean_accept_data(file_name):
+    data = []
+    with open(file_name) as f:
+        column_labels = f.readline().split("||")
+
+        for line in f:
+            d = line.split("||")
+            d[:4] = map(int, d[:4])
+            d[4:6] = map(float, d[4:6])
+            d[6:8] = map(int, d[6:8])
+            d[9:11] = map(int, d[9:11])
+            d[11] = float(d[11])
+            d[12] = int(d[12])
+            d[13] = datetime.strptime(d[13], "%Y-%m-%d %H:%M:%S")
+            d[14:16] = map(int, d[14:16])
+            d[18] = int(d[18])
+            d[19] = float(d[19])
+            d[20:26] = map(int, d[20:26])
+            d[27:37] = map(float, d[27:37])
+            d[38:40] = map(int, d[38:40])
+
+            data.append(d)
+
+    return column_labels, data
+
+
+def load_reject_data(file_name, state_dict):
     data = []
     with open(file_name) as f:
         column_labels = map(lambda x: x.strip("\" \n"), f.readline().split(","))
@@ -99,7 +126,7 @@ def load_reject_data(file_name):
                 d[0] = float(d[0])
                 d[3] = int(d[3])
                 d[4] = float(d[4][:-1])
-
+                d[6] = state_dict[d[6]]
                 if d[7] == "n/a" or d[7][0] == '<':
                     temp = 0
                 elif d[7][1] == '0':
@@ -112,10 +139,38 @@ def load_reject_data(file_name):
 
                 remove_reject_columns(d)
                 data.append(d)
-            except ValueError:
+            except (ValueError, KeyError):
                 pass
 
     return column_labels, data
+
+def load_cleaned_reject_data(file_name):
+    data = []
+    with open(file_name) as f:
+        column_labels = f.readline().split("||")
+
+        for line in f:
+            d = line.split("||")
+            d[0] = float(d[0])
+            d[1] = datetime.strptime(d[1], "%Y-%m-%d")
+            d[3] = int(d[3])
+            d[4] = float(d[4])
+            d[5:] = map(int, d[5:])
+
+            assert(len(d) == len(line.split("||")))
+
+            data.append(d)
+
+    return column_labels, data
+
+
+def write_file(file_name, data, headers=None, separator="||"):
+    with open(file_name, "w") as f:
+        if headers:
+            f.writelines(separator.join(headers) + '\n')
+
+        for row in data:
+            f.writelines(separator.join(map(str, row)) + '\n')
 
 
 def extract_column(data, index):
@@ -150,7 +205,17 @@ def generate_histograms(data, column_index, column_name, bin_counts=[10, 20, 30,
         py.show()
 
 
-def generate_normalized_state_histogram(data):
+def generate_normalized_state_histogram(data, column_index, state_dict, normalization_factor):
+    """
+
+    :param data:
+    :param column_index:
+    :param state_dict:
+    :param normalization_factor: Needed because the number of samples may be really small compared to population. Should
+    be 1000 for Accept data and about 250 for reject data
+    :return:
+    """
+    num_states = len(state_dict)
     state_populations_dict = {"AL": 4833722, "AK": 735132, "AZ": 6626624, "AR": 2959373, "CA": 38332521, "CO": 5268367,
                               "CT": 3596080, "DE": 925749, "DC": 646449, "FL": 19552860, "GA": 9992167, "HI": 1404054,
                               "ID": 1612136, "IL": 12882135, "IN": 6570902, "IA": 3090416, "KS": 2893957, "KY": 4395295,
@@ -159,61 +224,52 @@ def generate_normalized_state_histogram(data):
                               "NM": 2085287, "NY": 19651127, "NC": 9848060, "OH": 11570808, "OK": 3850568,
                               "OR": 3930065, "PA": 12773801, "RI": 1051511, "SC": 4774839, "SD": 844877, "TN": 6495978,
                               "TX": 26448193, "UT": 2900872, "VT": 626630, "VA": 8260405, "WA": 6971406, "WV": 1854304,
-                              "WI": 5742713, "WY": 582658}
+                              "WI": 5742713, "WY": 582658, "ME": 1328302, "ND": 723393}
 
-    state_dict = {"AL": 0, "AK": 1, "AZ": 2, "AR": 3, "CA": 4, "CO": 5, "CT": 6, "DE": 7, "DC": 8, "FL": 9, "GA": 10,
-                  "HI": 11, "ID": 12, "IL": 13, "IN": 14, "IA": 15, "KS": 16, "KY": 17, "LA": 18, "MD": 19, "MA": 20,
-                  "MI": 21, "MN": 22, "MS": 23, "MO": 24, "MT": 25, "NE": 26, "NV": 27, "NH": 28, "NJ": 29, "NM": 30,
-                  "NY": 31, "NC": 32, "OH": 33, "OK": 34, "OR": 35, "PA": 36, "RI": 37, "SC": 38, "SD": 39, "TN": 40,
-                  "TX": 41, "UT": 42, "VT": 43, "VA": 44, "WA": 45, "WV": 46, "WI": 47, "WY": 48}
+    assert (num_states == len(state_populations_dict))
 
-    histogram_data = extract_column(data, 18)
-    a, b = np.histogram(histogram_data, np.arange(50) - 0.5)
+    histogram_data = extract_column(data, column_index)
+    a, b = np.histogram(histogram_data, np.arange(num_states + 1) - 0.5)
 
     print a
     print sum(a)
 
     normalized = []
     for state in state_dict.keys():
-        normalized_value = a[state_dict[state]]*1000 / float(state_populations_dict[state])
+        normalized_value = a[state_dict[state]] * normalization_factor / float(state_populations_dict[state])
         normalized.append(normalized_value)
 
-    py.bar(range(49), normalized, align='center')
-    py.xticks(range(50))
-    py.xlim([-1, 50])
+    py.bar(range(num_states), normalized, align='center')
+    py.xticks(range(num_states))
+    py.xlim([-1, num_states + 1])
     py.ylim([0, 1])
 
     py.show()
 
 
 def main():
-    col, data = load_accept_data("LoanStats3b.csv")
+    state_dict = {"AL": 0, "AK": 1, "AZ": 2, "AR": 3, "CA": 4, "CO": 5, "CT": 6, "DE": 7, "DC": 8, "FL": 9, "GA": 10,
+                  "HI": 11, "ID": 12, "IL": 13, "IN": 14, "IA": 15, "KS": 16, "KY": 17, "LA": 18, "MD": 19, "MA": 20,
+                  "MI": 21, "MN": 22, "MS": 23, "MO": 24, "MT": 25, "NE": 26, "NV": 27, "NH": 28, "NJ": 29, "NM": 30,
+                  "NY": 31, "NC": 32, "OH": 33, "OK": 34, "OR": 35, "PA": 36, "RI": 37, "SC": 38, "SD": 39, "TN": 40,
+                  "TX": 41, "UT": 42, "VT": 43, "VA": 44, "WA": 45, "WV": 46, "WI": 47, "WY": 48, "ME": 49, "ND": 50}
 
+    col, data = load_clean_accept_data("cleanedAcceptData.csv")
 
     # accept histograms
     # -----------------
-    # generate_histograms(data, 0, col[0])
-    # generate_histograms(data, 11, col[11], remove_extreme_values=True, remove_count=1000, remove_from_lower=False)
-    # generate_histograms(data, 22, col[22])
-    # generate_histograms(data, 18, col[18], bin_counts=[np.arange(50) - 0.5])
+    generate_histograms(data, 0, col[0])
+    generate_histograms(data, 11, col[11], remove_extreme_values=True, remove_count=1000, remove_from_lower=False)
+    generate_histograms(data, 22, col[22])
     generate_histograms(data, 9, col[9], bin_counts=[np.arange(12) - 0.5], normalize=True)
+    generate_normalized_state_histogram(data, 18, state_dict, 1000)
 
+    col, data = load_cleaned_reject_data("cleanedRejectData.csv")
 
     # reject histograms
     # -----------------
-    # col, data = load_reject_data("RejectStatsA.csv")
-    # generate_histograms(data, 6, col[6], bin_counts=[np.arange(12) - 0.5], normalize=True)
-
-    # blah = set()
-    # for row in data:
-    #     blah.add(row[9])
-    #
-    # print blah
-    #
-    # d = {a: b for a, b in zip(blah, range(len(blah)))}
-    # print d
-
-    # print len(load_reject_data("RejectStatsA.csv"))
+    generate_histograms(data, 6, col[6], bin_counts=[np.arange(12) - 0.5], normalize=True)
+    generate_normalized_state_histogram(data, 5, state_dict, 200)
 
 
 if __name__ == "__main__":
