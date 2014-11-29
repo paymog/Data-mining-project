afe2cda4fee537ba2265db8909f36ceb6d3496cf
unknown <laurenslusky@gmail.com>
2014-11-29 11:05:41 -0600

    oragnized code, put all main code into a main()

--- apriori_analysis.py
+++ apriori_analysis.py
@@ -31,21 +31,24 @@ def get_col(col_names, data, extract):
     return np.array(filtered_data).T
 
 
+def main():
+    col, data = analysis.load_clean_accept_data("cleanedAcceptData.csv")
 
-col, data = analysis.load_clean_accept_data("cleanedAcceptData.csv")
+    filtered_data = get_col(col, data, [0, 8, 11, 16])
 
-filtered_data = get_col(col, data, [0, 8, 11, 16])
+    # just to test
+    print("First 10 rows of filtered data:\n")
+    i = 0
+    for i in range(0, 10):
+        print(filtered_data[i])
 
-# just to test
-print("First 10 rows of filtered data:\n")
-i = 0
-for i in range(0, 10):
-    print(filtered_data[i])
+    print("Frequent itemsets:\n")
+    # Find frequent itemsets - apriorialg.py
+    L, support_data = ap.apriori(filtered_data, 0.007)
 
-print("Frequent itemsets:\n")
-# Find frequent itemsets - apriorialg.py
-L, support_data = ap.apriori(filtered_data, 0.007)
+    # Generate rules - apriorialg.py
+    rules = ap.generateRules(L, support_data, 0.50)
+    print_rules(rules)
 
-# Generate rules - apriorialg.py
-rules = ap.generateRules(L, support_data, 0.50)
-print_rules(rules)
+
+main()
