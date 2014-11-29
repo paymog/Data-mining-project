baaf34a00d6c1d87c51d0b20c9c0e06f4b424435
unknown <laurenslusky@gmail.com>
2014-11-29 10:57:01 -0600

    Removed some code I wasn't using and have started my analysis write up. Running a more fine-grained run of data, will finish typing up results in a few hours when that it done

--- apriori_analysis.py
+++ apriori_analysis.py
@@ -15,51 +15,6 @@ def print_rules(rules):
         print("%s ---> %s conf: %f" % (antecedent, conseq, conf))
 
 
-def lift(rules):
-    # lift = P(Y | X) / P(Y)
-    for rule in rules:
-        antecedent = rule[0]
-        conseq = rule[1]
-        conf = rule[2]
-        lift = conf / support_data[conseq]
-        print("Lift value for %s ---> %s: %f" % (antecedent, conseq, lift))
-    print('\n')
-
-
-def interest(rules):
-    # interest = P(X , Y) / (P(X) * P(Y))
-    for rule in rules:
-        antecedent = rule[0]
-        conseq = rule[1]
-        unioned_set = antecedent.union(conseq)
-        interest = support_data[unioned_set] / (support_data[antecedent] * support_data[conseq])
-        print("Interest value for %s ---> %s: %f" % (antecedent, conseq, interest))
-    print('\n')
-
-
-def PS(rules):
-    # PS = P(X , Y) - (P(X) * P(Y))
-    for rule in rules:
-        antecedent = rule[0]
-        conseq = rule[1]
-        unioned_set = antecedent.union(conseq)
-        PS = support_data[unioned_set] - (support_data[antecedent] * support_data[conseq])
-        print("PS value for %s ---> %s: %f" % (antecedent, conseq, PS))
-    print('\n')
-
-
-def phi(rules):
-    # phi-coefficient = (P(X , Y) - (P(X) * P(Y))) / sqrt(P(X) * (1 - P(X)) * P(Y) * (1-P(Y)) )
-    for rule in rules:
-        antecedent = rule[0]
-        conseq = rule[1]
-        unioned_set = antecedent.union(conseq)
-        numerator = support_data[unioned_set] - (support_data[antecedent] * support_data[conseq])
-        denominator = m.sqrt(support_data[antecedent] * (1 - support_data[antecedent]) * support_data[conseq] * (1 - support_data[conseq]))
-        phi = numerator / denominator
-        print("phi-coefficient value for %s ---> %s: %f" % (antecedent, conseq, phi))
-    print('\n')
-
 def get_col(col_names, data, extract):
     filtered_data = []
     for col in extract:
@@ -89,7 +44,7 @@ for i in range(0, 10):
 
 print("Frequent itemsets:\n")
 # Find frequent itemsets - apriorialg.py
-L, support_data = ap.apriori(filtered_data, 0.02)
+L, support_data = ap.apriori(filtered_data, 0.007)
 
 # Generate rules - apriorialg.py
 rules = ap.generateRules(L, support_data, 0.50)
